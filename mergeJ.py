12f1088f5c272fec262292366cffaa7219e3c39f
hyunyong <antilogy33@gmail.com>
2019-01-18 14:37:07 +0900

    Update mergeJ.py

--- mergeJ.py
+++ mergeJ.py
@@ -1,15 +1,15 @@
 from PIL import Image
 import os, sys
 
-def mergeImg(imgL,fName, iType):
+def mergeImg(imgL,fName, iType, mr_):
   xL = []
   yL = []
-  mr = 100
+  mr = mr_
   for x in imgL:
     xL.append(x.size[0])
     yL.append(x.size[1])
-  newImg = Image.new("RGBA", (max(xL), sum(yL)+mr*len(yL)),(256,256,256))
-  mImg = Image.new("RGBA", (max(xL), mr),(256,256,256))
+  newImg = Image.new("RGB", (max(xL), sum(yL)+mr*len(yL)),(256,256,256))
+  mImg = Image.new("RGB", (max(xL), mr),(256,256,256))
   minY = 0
   maxY = 0
   for x in xrange(len(yL)):
@@ -23,15 +23,38 @@ def mergeImg(imgL,fName, iType):
     newImg.paste(mImg, (0, maxY, max(xL), maxY+mr))
     maxY += mr
     minY += mr
-  #newImg.save(fName+"."+iType, iType)
-  #newImg.thumbnail(, PyImage.ANTIALIAS)
   newImg.save(fName+".jpg",quality=95)
-  #newImg.save(fName+"."+iType, iType)
+
+def mergeImgRS(imgL,fName, iType, mr_, maxP):
+  xL = []
+  yL = []
+  mr = mr_
+  reL = []
+  for x in imgL:
+    x = x.resize((maxP,int(x.size[1]*float(maxP)/float(x.size[0]))), Image.ANTIALIAS)
+    reL.append(x)
+    xL.append(x.size[0])
+    yL.append(x.size[1])
+  newImg = Image.new("RGB", (maxP, sum(yL)+mr*len(yL)),(256,256,256))
+  mImg = Image.new("RGB", (max(xL), mr),(256,256,256))
+  minY = 0
+  maxY = 0
+  for x in xrange(len(yL)):
+    maxY += yL[x]
+    area = (0, minY, maxP, maxY)
+    print minY, maxY
+    minY += yL[x]
+    if  iType == "png":newImg.paste(reL[x], area, mask=reL[x])
+    else: newImg.paste(reL[x], area)
+    newImg.paste(mImg, (0, maxY, max(xL), maxY+mr))
+    maxY += mr
+    minY += mr
+  newImg.save(fName+".jpg",quality=95)
+
 
 inTag = sys.argv[1]
 fName, iType = inTag.split(".")
-
+mr_ = int(sys.argv[2])
 imL = [Image.open(x) for x in os.listdir(".") if x.endswith(iType)]
 imL.sort()
-mergeImg(imL, fName, iType)
-~
+mergeImgRS(imL, fName, iType, mr_, 860)
